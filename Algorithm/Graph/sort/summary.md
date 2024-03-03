df8817c3f5c8e75c47c24c1df58ed9a0b2963802
Najah7 <najahmatsuo7@gmail.com>
2024-03-03 09:50:21 +0900

    ✨ [Done] Topological Sort (Graph Algorithm)

---
+++ Algorithm/Graph/sort/summary.md
@@ -0,0 +1,4 @@
+# Topological Sort
+
+## Description
+Sorts given actions in such a way that if there is a dependency of one action on another, then the dependent action always comes later than its parent action.
---
+++ Algorithm/Graph/sort/topological_sort.py
@@ -0,0 +1,43 @@
+import typing as t
+from collections import defaultdict
+
+class Graph:
+    
+    def __init__(self, num_vertices):
+        self.graph = defaultdict(list)
+        self.num_vertices = num_vertices
+
+    def add_edge(self, from_vertex: str, to_vertex: str):
+        self.graph[from_vertex].append(to_vertex)
+    
+    def topological_sort(self): # O(vertex + edge)
+        visited = set()
+        stack = []
+        for vertex in list(self.graph): # O(vertex)
+            if vertex not in visited:
+                self._topological_sort_explore(vertex, visited, stack) # O(edge)
+        return stack
+    
+    def _topological_sort_explore(
+            self,
+            vertex: str,
+            visited: t.Set[str],
+            stack: t.List[str]
+        ): # O(edge)
+        visited.add(vertex)
+        for neighbor in self.graph[vertex]: # O(edge)
+            if neighbor not in visited:
+                self._topological_sort_explore(neighbor, visited, stack)
+        stack.insert(0, vertex)
+
+graph = Graph(8)
+graph.add_edge("A", "C")
+graph.add_edge("C", "E")
+graph.add_edge("E", "H")
+graph.add_edge("E", "F")
+graph.add_edge("F", "G")
+graph.add_edge("B", "D")
+graph.add_edge("B", "C")
+graph.add_edge("D", "F")
+print(graph.topological_sort())
+
